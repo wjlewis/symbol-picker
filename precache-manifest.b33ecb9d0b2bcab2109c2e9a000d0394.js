ccf3da2c5acadc7ff0706137148fbd2e41b326f9
wjl <wjl@system76-pc.localdomain>
2020-04-02 18:35:33 -0400

    Updates

---
+++ precache-manifest.b33ecb9d0b2bcab2109c2e9a000d0394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7bdafa5e9a3bc2356e5903eededf48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bbd04fcab91b8411d18",
+    "url": "/static/css/main.dab1e223.chunk.css"
+  },
+  {
+    "revision": "7dcfb5bd33ed68293031",
+    "url": "/static/js/2.163957ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.163957ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbd04fcab91b8411d18",
+    "url": "/static/js/main.e4b0efd9.chunk.js"
+  },
+  {
+    "revision": "eefb5e951940c6947e28",
+    "url": "/static/js/runtime-main.a7850fc8.js"
+  }
+]);
